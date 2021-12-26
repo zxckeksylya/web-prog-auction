59b41260928b54cafde0fa735c06c92037de6071
Gridin Egor <egorka.gridin@gmail.com>
2021-12-26 17:37:49 +0300

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { CatalogComponent } from './catalog/catalog.component';
+import { ProdCardComponent } from './prod-card/prod-card.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [];
 
@@ -9,6 +13,10 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot([
     {path:"main",component:MainPageComponent},
     {path:"catalog",component:CatalogComponent},
+    {path:"prod-card",component:ProdCardComponent},
+    {path:"login",component:LoginComponent},
+    {path:"register",component:RegisterComponent},
+    {path:"profile",component:ProfileComponent},
     {path:"**",redirectTo:"main"}
   ])],
   exports: [RouterModule]
